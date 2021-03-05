4dab372135628dc92beebf3eee4ec09cd21cd66c
Jaber Shamali <jaber_sha@hotmail.com>
2021-03-04 21:46:41 -0300

    Add element Loading and Network validation

--- Marvel/View/ViewController.swift
+++ Marvel/View/ViewController.swift
@@ -7,29 +7,32 @@
 
 import UIKit
 import Alamofire
+import Network
+
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var Collection: UICollectionView!
     @IBOutlet weak var Tabela: UITableView!
+    @IBOutlet weak var loader: UIActivityIndicatorView!
     
     var Data = [Entry]()
     let request = API()
-    var imagemTeste = [UIImage]()
     var from = 0
-
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "InternetConnectionMonitor")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        request.requestData(from: nil) { (result) in
-            self.Data.append(result)
-            self.Tabela.reloadData()
-            self.Collection.reloadData()
-        }
-
-        
+        DispatchQueue.main.async(execute: {
+            self.monitor.start(queue: self.queue)
+            self.checkNetwork()
+        })
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loader.startAnimating()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -49,8 +52,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         AF.request(url ,method: .get).response{ response in
          switch response.result {
           case .success(let responseData):
+            self.loader.stopAnimating()
+            self.loader.isHidden = true
             cell.img.image = UIImage(data: responseData!, scale:1)
-
           case .failure(let error):
               print("error--->",error)
           }
@@ -77,9 +81,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
          switch response.result {
           case .success(let responseData):
             cell.img.image = UIImage(data: responseData!, scale:1)
-            self.imagemTeste.append(UIImage(data: responseData!, scale:1)!)
           case .failure(let error):
-              print("error--->",error)
+            print("error--->",error)
           }
         }
 
@@ -108,7 +111,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         from += 15
         request.requestData(from: from) { (result) in
             self.Data[0].data.result.append(contentsOf: result.data.result)
-            print("Total: \(self.from)")
         }
     }
     
@@ -135,12 +137,44 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         if let indexPath = Tabela.indexPathForSelectedRow{
             
             let detailVC = segue.destination as! DescriptionViewController
-            let url = convertURL(link: Data[0].data.result[indexPath.row+5].thumb.path, extensionImg: Data[0].data.result[indexPath.row].thumb.extensionThumb, type: "SV")
+            let url = convertURL(link: Data[0].data.result[indexPath.row+5].thumb.path, extensionImg: Data[0].data.result[indexPath.row+5].thumb.extensionThumb, type: "SV")
             
             detailVC.name = Data[0].data.result[indexPath.row+5].name
             detailVC.descriptionCharacters = Data[0].data.result[indexPath.row+5].description
             detailVC.imgUrl = url
             }
+    }
+    
+    func showAlert(){
+        // create the alert
+               let alert = UIAlertController(title: "Aviso", message: "Você está sem acesso a internet!!!", preferredStyle: UIAlertController.Style.alert)
+
+               // add the actions (buttons)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { action in
+            self.checkNetwork()
+        }))
+        
+        
+
+        DispatchQueue.main.async(execute: {
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        })
+        
+    }
+    
+    func checkNetwork(){
+        monitor.pathUpdateHandler = { pathUpdateHandler in
+            if pathUpdateHandler.status == .satisfied {
+                self.request.requestData(from: nil) { (result) in
+                self.Data.append(result)
+                self.Tabela.reloadData()
+                self.Collection.reloadData()
+                }
+            } else {
+                self.showAlert()
+            }
+        }
     }
     
 
